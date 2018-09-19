6f758702d76a120fa744dacbd75de21b36937f41
enrique-AMT <enrique.marrero1@upr.edu>
2018-09-19 18:03:49 -0400

    small commit, initial work
    Some methods for testing networkX added. Might still not work natively with the included packages for now.

---
+++ BEAR001a.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------------------------
+# BEAR - Version 0.01a
+#
+# Social Network Analysis Language.
+# -----------------------------------------------------------------------------
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G= nx.Graph()
+G.add_nodes_from([1,5])
+print (list(G.nodes))
