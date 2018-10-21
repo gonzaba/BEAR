9bf2836ab08b228fdf322386b5961c24c54b3765
enrique-AMT <enrique.marrero1@upr.edu>
2018-10-21 15:42:02 -0400

    added some tokens

--- BEAR001a.py
+++ BEAR001a.py
@@ -6,7 +6,19 @@
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import ply.lex as lex
 
+tokens = ( 'ADD' , 'DEFINE' , 'FILE' , 'FUNCTION' , 'DISPLAY' , 'DIGIT' , 'CHARACTER' , 'DELIMITERS' )
+
+#Tokens
+
+t_DIGIT = r'[0-9]+'
+t_CHARACTER = r'[a-zA-Z-_.]+'
+t_DELIMITERS= '//' | '\\' | '|' | '(' | ')' | '[' | ']' | '{' | '}'
+
+
+# Ignore tabs
+t_ignore = " \t"
 G= nx.Graph()
 G.add_nodes_from([1,5])
 print (list(G.nodes))
