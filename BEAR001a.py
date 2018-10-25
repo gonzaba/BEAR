0dc18c18f84dea6bde63328a670d681b7a53874d
PentiumFallen <raul.vargas@upr.edu>
2018-10-25 16:08:30 -0400

    Update BEAR001a

--- BEAR001a.py
+++ BEAR001a.py
@@ -8,21 +8,32 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import ply.lex as lex
 
-tokens = ( 'ADD' , 'DEFINE' , 'FILE' , 'FUNCTION' , 'DISPLAY' , 'DIGIT' , 'CHARACTER' , 'DELIMITERS' )
+tokens = ('DIGIT' , 'CHARACTER' , 'DELIMITERS' )
 
 #Tokens
 
 t_DIGIT = r'[0-9]+'
 t_CHARACTER = r'[a-zA-Z-_.]+'
-t_DELIMITERS= '//' | '\\' | '|' | '(' | ')' | '[' | ']' | '{' | '}'
 
+# Delimiters unimplemented for now, checking for a solution... ~Enrique
+# t_DELIMITERS= '//' | '\\' | '|' | '(' | ')' | '[' | ']' | '{' | '}'
 
+# Parser
+import ply.yacc as yacc
+# THIS PART IS HIGHLY WIP FOR NOW. IT'S UNRELIABLE! ~Enrique
+def p_add(p):
+    '''node + [ file , node ] '''
+def p_define(p):
+  '''  'BEAR' ~ function '''
 # Ignore tabs
 t_ignore = " \t"
-G= nx.Graph()
-G.add_nodes_from([1,5])
-print (list(G.nodes))
+filename=input("Welcome to BEAR! Please enter your code's filename to compile. (NOTE: file must be in .cub format): \n")
+if(not filename.__contains__('.cub')):
+    print("unsupported file format!")
+print(filename)
 
-
+# HONEY class for auxiliary functions
+# class Honey (object):
+    #TODO: Add auxiliary operations!
 # hola
 # HI
