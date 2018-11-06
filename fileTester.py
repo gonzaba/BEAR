a2a18a05fada2ceaaa5ce91472121af2a5c3c208
alberto-canela <alberto.canela@upr.edu>
2018-11-06 14:57:08 -0400

    Update Phyton 3

--- fileTester.py
+++ fileTester.py
@@ -7,9 +7,11 @@ import networkx as nx
 
 #Create empty graph with no nodes and no edges
 G=nx.Graph()
+M =nx.Graph()
 
 #ask the user for the name of the file to use to create the graph
-fileName = raw_input("Complete File Name to use: \n")
+fileName = input("Complete File Name to use: \n")
+fileName2 = input("SECOND")
 
 #now its looking for the file
 file = open(fileName, 'r')
@@ -29,12 +31,43 @@ for line in file:
     # 7- Last Trip
     # 8-Place Last Visited
 
-        print person
+        #print(person)
         G.add_node(person[0], age = person[1], gender = person[2], birthplace = person[3], currentLocation = person[4], numVaccines = person[5], race = person[6], lastTrip = person[7], placeVisited = person[8])
+
 
 
 file.close()
 
 #print G.nodes(data=True)
+
+# now its looking for the file
+file = open(fileName2, 'r')
+
+# Read each individual line and create the person
+for line in file:
+    if line != '\n':
+        line = line.strip()
+        person = line.split(',')
+        # 0- Name
+        # 1- Age
+        # 2- Gender
+        # 3- Birth Place
+        # 4- Current Location
+        # 5- Number of Vaccines
+        # 6- Race
+        # 7- Last Trip
+        # 8-Place Last Visited
+
+        # print(person)
+        M.add_node(person[0], age=person[1], gender=person[2], birthplace=person[3], currentLocation=person[4],
+                   numVaccines=person[5], race=person[6], lastTrip=person[7], placeVisited=person[8])
+
+file.close()
+
+
+R =nx.Graph()
+R = nx.union(G,M)
+
+print (R.nodes(data=True))
 
 
